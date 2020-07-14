fca5613c41ae50d0ce4d2876f11bdfd0a6688b73
Bilkis Ismail <bilkisismail07@gmail.com>
2020-07-14 15:29:50 +0700

    add dot env + setting cors

---
+++ index.js
@@ -0,0 +1,46 @@
+require('dotenv').config()
+const { APP_PORT, APP_URL } = process.env
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+// Import Routes
+const home = require('./src/route/index')
+
+// Allowed All
+app.use(cors())
+
+// Allowed Url Origins
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:5000',
+  'http://127.0.0.1:5500'
+]
+
+// Allowed spesific
+app.use(cors({
+  origin: function (origin, callback) {
+    // (like mobile apps or curl requests)
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}))
+
+// Setting up bodyParser to use json and set it to req.body
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
+// App Routes
+app.use('/', home)
+
+// Run Server
+app.listen(APP_PORT || 8000, () => {
+  console.log(`Server run on port ${APP_PORT}`)
+  console.log(`Rest api URL:  ${APP_URL}:${APP_PORT}`)
+})
